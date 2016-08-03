3deefd1a8950da78145d75aa2ad9cac69984628b
Mateo Barraza <mbarraza@macadamian.com>
2016-08-02 22:20:33 -0700

    Debugging CI.

--- ios/OwalProtos/Scripts/build-protos.sh
+++ ios/OwalProtos/Scripts/build-protos.sh
@@ -3,6 +3,7 @@
 # Assuming we're in (something)/owal-mobile/ios/OwalProtos/...
 # and protos are in (something)/owal/src/protobuf/...
 PROTOS_DIR=${SRCROOT}/../../../owal/src/protobuf/
+pwd
 
 mkdir -p protos
 protoc --objc_out=protos --proto_path=${PROTOS_DIR} ${PROTOS_DIR}/*.proto
