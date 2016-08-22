69f5815bab7bc953dd3af3bc1ae20fcee2cd323d
Yakov Okshtein <yakov@owal.io>
2016-08-22 14:42:57 -0400

    Remove some more Facebook SDK things

--- android/app/src/main/java/io/owal/android/MainApplication.java
+++ android/app/src/main/java/io/owal/android/MainApplication.java
@@ -3,7 +3,6 @@ package io.owal.android;
 import android.app.Application;
 import android.util.Log;
 
-import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -30,20 +29,6 @@ public class MainApplication extends Application implements ReactApplication {
       );
     }
   };
-
-  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
-
-  protected static CallbackManager getCallbackManager() {
-    return mCallbackManager;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-
-    // If you want to use AppEventsLogger to log events.
-    AppEventsLogger.activateApp(this);
-  }
 
   @Override
   public ReactNativeHost getReactNativeHost() {
