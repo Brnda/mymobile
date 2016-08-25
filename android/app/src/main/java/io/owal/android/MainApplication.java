9ec9bc6a3e94bbba031e8f458a8bab2efb4d512b
Yakov Okshtein <yakov@owal.io>
2016-08-25 11:25:17 -0400

    Manually link new packages into React application

--- android/app/src/main/java/io/owal/android/MainApplication.java
+++ android/app/src/main/java/io/owal/android/MainApplication.java
@@ -9,6 +9,8 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
+import com.eguma.barcodescanner.BarcodeScannerPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,7 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new AnExampleReactPackage()
+          new AnExampleReactPackage(),
+          new RCTCameraPackage(),
+          new BarcodeScannerPackage()
       );
     }
   };
