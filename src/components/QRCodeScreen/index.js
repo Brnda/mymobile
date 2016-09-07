a3f83714b4569ba884f06c4aa66d4ddc0f17233d
Mateo Barraza <mateo.barraza@gmail.com>
2016-09-07 15:46:05 -0700

    Fix: The Camera view will only fire the reading of the bar code once and not multiple times.

--- src/components/QRCodeScreen/index.js
+++ src/components/QRCodeScreen/index.js
@@ -11,8 +11,13 @@ import {
 import Camera from 'react-native-camera';
 import styles from './styles';
 import {Actions} from 'react-native-router-flux';
+import _ from 'lodash';
 
 class QRCodeScreen extends Component {
+  constructor(props) {
+    super(props);
+    this._onBarCodeRead = _.once(this._onBarCodeRead.bind(this));
+  }
   _onBarCodeRead(result) {
     (Platform.OS === 'ios') ? VibrationIOS.vibrate() : Vibration.vibrate();
     this.props.onSelect(result.data);
@@ -24,7 +29,7 @@ class QRCodeScreen extends Component {
 
   render() {
     return (
-        <Camera onBarCodeRead={this._onBarCodeRead.bind(this)} style={styles.camera}>
+        <Camera onBarCodeRead={this._onBarCodeRead} style={styles.camera}>
           <View style={styles.rectangleContainer}>
             <View style={styles.rectangle}/>
           </View>
