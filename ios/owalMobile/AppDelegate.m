9c449a453eb3ca7e896abbea5083fc9b198bc68d
Mateo Barraza <mateo.barraza@gmail.com>
2016-09-12 20:48:59 -0700

    Upgrade to React Native 0.33 latest.

--- ios/owalMobile/AppDelegate.m
+++ ios/owalMobile/AppDelegate.m
@@ -16,10 +16,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-
   NSURL *jsCodeLocation;
 
-  [[RCTBundleURLProvider sharedSettings] setDefaults];
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
