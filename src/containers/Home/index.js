63f185f86b66850ec24c184f91dfbc18f750bac9
Mateo Barraza <mateo.barraza@gmail.com>
2016-09-19 14:15:46 -0700

    Interface implementation for the Home screen.

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -5,7 +5,19 @@ import styles from './styles';
 export default Home = () => {
   return (
       <View style={styles.container}>
-        <Text>Home</Text>
+        <View style={{backgroundColor: 'yellow', alignItems: 'center', marginTop: 50}}><Text>SPACES</Text></View>
+        <View style={{backgroundColor: 'red', margin: 10, flexDirection: 'row', flex: 1}}>
+          <View style={{backgroundColor: 'blue', flex: 1, margin: 20, alignItems: 'center'}}><Text>MAIN ENTRANCE</Text></View>
+          <View style={{backgroundColor: 'green', flex: 1, margin: 20, alignItems: 'center'}}><Text>MY FLOOR</Text></View>
+        </View>
+        <View style={{backgroundColor: 'red', margin: 10, flexDirection: 'row', flex: 1}}>
+          <View style={{backgroundColor: 'blue', flex: 1, margin: 20, alignItems: 'center'}}><Text>LAUNDRY</Text></View>
+          <View style={{backgroundColor: 'green', flex: 1, margin: 20, alignItems: 'center'}}><Text>GYM</Text></View>
+        </View>
+        <View style={{backgroundColor: 'red', margin: 10, flexDirection: 'row', flex: 1}}>
+          <View style={{backgroundColor: 'blue', flex: 1, margin: 20, alignItems: 'center'}}><Text>POOL</Text></View>
+          <View style={{backgroundColor: 'green', flex: 1, margin: 20, alignItems: 'center'}}><Text>GARAGE</Text></View>
+        </View>
       </View>
   );
 }
