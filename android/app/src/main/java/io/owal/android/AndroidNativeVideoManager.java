b489d49df65127170ecf058ed705d090f37ce039
Yakov Okshtein <yakov@owal.io>
2016-09-27 12:11:26 -0400

    Remove logging

--- android/app/src/main/java/io/owal/android/AndroidNativeVideoManager.java
+++ android/app/src/main/java/io/owal/android/AndroidNativeVideoManager.java
@@ -1,9 +1,7 @@
 package io.owal.android;
 
-import android.app.Activity;
 import android.net.Uri;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.widget.VideoView;
 
 import com.facebook.react.uimanager.SimpleViewManager;
@@ -12,7 +10,6 @@ import com.facebook.react.uimanager.annotations.ReactProp;
 
 public class AndroidNativeVideoManager extends SimpleViewManager<VideoView> {
 
-  private static final String TAG = AndroidNativeVideoManager.class.getSimpleName();
 
   @Override
   public String getName() {
@@ -21,14 +18,11 @@ public class AndroidNativeVideoManager extends SimpleViewManager<VideoView> {
 
   @Override
   protected VideoView createViewInstance(ThemedReactContext reactContext) {
-    Log.i(TAG, "Creating new view instance!");
     return new VideoView(reactContext);
   }
 
   @ReactProp(name = "src")
   public void setSrc(VideoView view, @Nullable String src) {
-    Log.i(TAG, "Setting src: " + src);
-    Log.d(TAG, "wxh:" + view.getWidth() + " x " + view.getHeight());
     if (view.isPlaying()) {
       view.stopPlayback();
     }
