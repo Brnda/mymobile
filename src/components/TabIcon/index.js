25334e45ba36b23ee4af8879c55c70907344ed54
Mateo Barraza <mateo.barraza@gmail.com>
2016-10-27 16:07:16 -0700

    Added correct icons to Home screen tab bar, also corrected the color of the tabbar to match design

--- src/components/TabIcon/index.js
+++ src/components/TabIcon/index.js
@@ -6,11 +6,11 @@ import {
 const TabIcon = (props) => {
   switch(props.iconName) {
     case 'buildingEntrance':
-      return <Image source={require('../../icons/building.png')} />;
+      return <Image source={require('../../icons/buildingW.png')} />;
     case 'message':
-      return <Image source={require('../../icons/message.png')} />;
+      return <Image source={require('../../icons/messageW.png')} />;
     case 'profile':
-      return <Image source={require('../../icons/profile.png')} />;
+      return <Image source={require('../../icons/profileW.png')} />;
     default:
       return null;
   }
