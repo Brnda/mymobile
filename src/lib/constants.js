fb98386b21280cc01201db1319d6a58d1a93a4b3
Mateo Barraza <mateo.barraza@gmail.com>
2016-11-03 18:00:55 -0700

    Completed work for web socket connector to push real time update to spaces.

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,19 +1,22 @@
 const APP_CONST_PROD = {
   BaseUrl: 'owal-api.herokuapp.com',
-  Port: '80'
+  Port: '80',
+  PortWS: 3000
 };
 
 // Local server
 const APP_CONST_LOCALDEV = {
   BaseUrl: '192.168.0.78',
   // BaseUrl: '127.0.0.1',
-  Port: '3030'
+  Port: '3030',
+  PortWS: 3000
 };
 
 export const SKIP_INDUCTION_KEY = '@AsyncStorage:skipInduction';
 export const USER_TOKEN = '@AsyncStorage:userToken';
+export const TENANT_ID = '@AsyncStorage:tenantID';
 
 // DO NOT SUBMIT IF UNCOMMENTED
 //export default APP_CONST_LOCALDEV;
 
-export default APP_CONST_PROD;
+export default APP_CONST_LOCALDEV;
